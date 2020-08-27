330457573223fd3718ba93cbb198dd9d717916fe
FanPu1 <fanpu.cs@gmail.com>
2020-08-27 00:08:18 -0500

    master: revert jquery

--- docs/js/displayIframe.js
+++ docs/js/displayIframe.js
@@ -1,8 +1,14 @@
 $( document ).ready(function() { //https://learn.jquery.com/using-jquery-core/document-ready/
-  $("#national-park-html").hide();
-  $("#national-park").mouseenter( 
-    ()=>{$("#national-park-html").show(500);}
-   ).mouseleave( 
-    // do nothing
-  );
+  //$("#national-park-html").hide();
+  $("#loading-html").ready(()=>{
+    console.log("loading");
+    $("#national-park").mouseenter( 
+      ()=>{
+        $("#national-park-html").show(500);
+        console.log("mourse in");
+      }
+     ).mouseleave( 
+      // do nothing
+    );
+  });
 });
